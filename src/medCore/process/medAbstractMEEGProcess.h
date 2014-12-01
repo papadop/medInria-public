cebe5568f6a5e8b418090bf0ec18c4548ac4e30e
Theodore Papadopoulo <Theodore.Papadopoulo@inria.fr>
2014-12-01 14:36:10 +0100

    Created a first canvas of medAbstractMEEGProcess.

---
+++ src/medCore/process/medAbstractMEEGProcess.h
@@ -0,0 +1,31 @@
+/*=========================================================================
+
+ medInria
+
+ Copyright (c) INRIA 2013 - 2014. All rights reserved.
+ See LICENSE.txt for details.
+ 
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+=========================================================================*/
+
+#pragma once
+
+#include <medCoreExport.h>
+#include <medAbstractProcess.h>
+
+class medAbstractData;
+
+class MEDCORE_EXPORT medAbstractMEEGProcess: public medAbstractProcess {
+
+    Q_OBJECT
+
+public:
+
+    medAbstractMEEGProcess(medAbstractProcess*parent=NULL): medAbstractProcess(parent) { }
+    virtual ~medAbstractMEEGProcess() { }
+
+    virtual void setInputImage(medAbstractData *data) = 0;
+};
